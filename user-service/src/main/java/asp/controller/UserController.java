7a04aff9848c3e078cd3bcd1d9b16341d216d198
Aenao17 <gabrieloanea17@gmail.com>
2025-05-20 21:04:05 +0300

    Swagger done

--- user-service/src/main/java/asp/controller/UserController.java
+++ user-service/src/main/java/asp/controller/UserController.java
@@ -3,6 +3,8 @@ package asp.controller;
 import asp.model.User;
 import asp.service.AuthenticationService;
 import asp.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "Utilizatori", description = "Operații cu utilizatori")
 public class UserController {
 
     private final UserService userService;
@@ -24,6 +27,7 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "Afișează toți utilizatorii (necesită autentificare)")
     public List<User> getAllUsers() {
         return userService.findAllUsers();
     }
